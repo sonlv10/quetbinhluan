e54560913af678dc109ecd4688653ab7fc425b4d
sonle <son.le@hdwebsoft.co>
2021-10-17 18:01:38 +0700

    update layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,20 +2,30 @@
 module.exports = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'quetbinhluan-app',
+    title: 'Quét bình luận | Công cụ miễn phí hỗ trợ bán hàng online',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
+      { name: 'author', content: 'FAF'},
+      { name: 'robots', content: 'index, follow'},
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'description', name: 'description', content: 'Một phần mềm miễn phí giúp tiết kiệm thời gian và nhân lực đáng kể trong thời đại số hiện nay.' },
       { name: 'title', content: 'Quét bình luận | Công cụ miễn phí hỗ trợ bán hàng online' },
       { name: 'keywords', content: 'quét facebook, miễn phí, quét bình luận, quét comment, quét bài viết, auto facebook, scan bài viết, scan post, quét live stream, quét số điện thoại' },
       { name: 'format-detection', content: 'telephone=no' },
+      { property: 'og:description', name: 'description', content: 'Một phần mềm miễn phí giúp tiết kiệm thời gian và nhân lực đáng kể trong thời đại số hiện nay.' },
+      { property: 'og:title', content: 'Quét bình luận | Công cụ miễn phí hỗ trợ bán hàng online' },
+      { property: 'og:type', content: 'article' },
+      { property: 'og:url', content: 'https://quetbinhluan.com' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
     ]
   },
 
