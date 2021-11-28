0694857c26068268715516421b59dc5b3ed4722c
sonle <son.le@hdwebsoft.co>
2021-11-28 15:51:58 +0700

    upload urrl

--- components/Home.vue
+++ components/Home.vue
@@ -15,7 +15,7 @@
                 <div class="flex w-full justify-center md:justify-start pb-24 lg:pb-0 fade-in">
                     <div class="wrapper w-full">
                         <div class="btns flex">
-                            <a href="https://api.quetbinhluan.com/download/fake-app-0.1.0-mac.dmg.zip">Tải về (Mac OS)</a>
+                            <a href="https://api.quetbinhluan.com/download/fake-app-0.1.0.dmg.zip">Tải về (Mac OS)</a>
                             <a href="https://app.quetbinhluan.com/" target="_blank">Bản web</a>
                         </div>
                     </div>
