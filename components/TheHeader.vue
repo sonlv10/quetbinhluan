34262b38a05013efa8b267457d96d73f2028f6f8
sonle <son.le@hdwebsoft.co>
2021-11-29 21:54:42 +0700

    add contact

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,5 +2,8 @@
   <header class="flex">
     <NuxtLink to="/"><Logo class="mb-4" /></NuxtLink>
     <AppSearchInput class="ml-8" />
+    <div>
+      <a class="text-blue-500 font-bold ml-8" href="https://www.facebook.com/107373678449616" target="_blank">Liên hê</a>
+    </div>
   </header>
 </template>
