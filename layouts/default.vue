c0faa9dd8a616509c428132fe89eed0ea444576a
sonle <son.le@hdwebsoft.co>
2021-12-16 11:30:29 +0700

    revert code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,58 +1,26 @@
 <template>
-<div>
-    <!-- Messenger Plugin chat Code -->
-    <div id="fb-root"></div>
-
-    <!-- Your Plugin chat code -->
-    <div id="fb-customer-chat" class="fb-customerchat"></div>
+  <div>
     <Nuxt />
-</div>
+  </div>
 </template>
 
 <style>
 html {
-    font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-        Roboto, "Helvetica Neue", Arial, sans-serif;
-    font-size: 16px;
-    word-spacing: 1px;
-    -ms-text-size-adjust: 100%;
-    -webkit-text-size-adjust: 100%;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    box-sizing: border-box;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
 }
 
 *,
 *::before,
 *::after {
-    box-sizing: border-box;
-    margin: 0;
+  box-sizing: border-box;
+  margin: 0;
 }
 </style>
-
-<script>
-var chatbox = document.getElementById("fb-customer-chat");
-chatbox.setAttribute("page_id", "107373678449616");
-chatbox.setAttribute("attribution", "biz_inbox");
-</script>
-
-<script>
-<!-- Your SDK code -->
-
-window.fbAsyncInit = function () {
-    FB.init({
-        xfbml: true,
-        version: "v12.0"
-    });
-};
-
-(function (d, s, id) {
-    var js,
-        fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
-    fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
-</script>
