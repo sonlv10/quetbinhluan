b75f8b43caf4e5ccb25f552fead29c37d8096a82
sonle <son.le@hdwebsoft.co>
2021-12-17 10:12:11 +0700

    add messenger

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vue-fb-customer-chat.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
