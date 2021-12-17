fe7a8deafe5c023ba209f4f419ff8b0969eb477a
sonle <son.le@hdwebsoft.co>
2021-12-17 10:29:04 +0700

    update thêm color

--- plugins/vue-fb-customer-chat.js
+++ plugins/vue-fb-customer-chat.js
@@ -3,6 +3,6 @@ import VueFbCustomerChat from 'vue-fb-customer-chat'
  
 Vue.use(VueFbCustomerChat, {
   page_id: 107373678449616, //  change 'null' to your Facebook Page ID,
-  theme_color: '#be6ceb', // theme color in HEX
+  theme_color: null, // theme color in HEX
   locale: 'vi', // default 'vi'
 })
